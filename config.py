484d43059b8574d919026303a62cc9bfedfa9e9b
autofollower350 <followerauto350@gmail.com>
2025-06-09 17:01:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "28590286") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "6a68cc6b41219dc57b7a52914032f92f") #Replace With your api hash
+API_ID = os.environ.get("API_ID", "24085134") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "2a8b84b87c8fb9c726f200e6af0d5144") #Replace With your api hash
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Replace With your bot token
